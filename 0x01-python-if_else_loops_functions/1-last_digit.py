e0ea627b673681c93d6e3ca715c1ab816c99938a
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-11-29 19:13:00 -0800

    task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if (number < 0):
-    lastDig = (-1) * (number%10)
-else:
-    lastDig = number%10
-print(f"Last digit of {number} is {lastDig}", end = " ")
+lastDig = number % 10
+print(f"Last digit of {number} is {lastDig}", end=" ")
 if (lastDig > 5):
     print("and is greater than 5")
 elif (lastDig == 0):

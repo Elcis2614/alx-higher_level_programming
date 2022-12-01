3b503d45b88e5d1f170c91b4770153f1b21a4484
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-12-01 11:48:14 -0800

    more task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-from add_0 import add as addition
-a = 1
-b = 2
-c = addition(a, b)
-print("{x} + {y} = {z}".format(x=a, y=b, z=c))
+if __name__ == "__main__":
+    from add_0 import add as addition
+    a = 1
+    b = 2
+    print("{x} + {y} = {z}".format(x=a, y=b, z=addition(a, b)))

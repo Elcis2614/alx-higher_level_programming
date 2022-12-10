c91c6d0ea37f7ccf884ec296e232ca8fb6d2bc0d
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-12-10 04:00:33 -0800

    power

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -3,10 +3,10 @@ def pow(a, b):
     if (b != 0):
         t = a
         if(b > 0):
-            for i in range(b):
+            for i in range(b - 1):
                 a *= t
         else:
-            for i in range(abs(b)):
+            for i in range(abs(b) - 1):
                 a /= t
         return a
     elif (b == 0 and a != 0):

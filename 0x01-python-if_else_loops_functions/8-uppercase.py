e6974ba188cd4205c016e512f587ab22f341066d
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-12-10 01:06:00 -0800

    string to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    s=""
+    if(str != ""):
+        for i in range(len(str)):
+            x = ord(str[i])
+            if(x >= ord('a') and x <= ord('z')):
+                s += chr(x - 32)
+            else:
+                s += str[i]
+        print(s)

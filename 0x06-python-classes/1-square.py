e0dff75133dc64f5dd421f494815aa08bb1eb8b0
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-12-20 07:19:29 -0800

    first attribute to the square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module contains a code for
+a class with one private attribute, i just hope it will work"""
+
+
+class Square:
+    """This class defines the square"""
+    def __init__(self, s):
+        self.__size__ = s

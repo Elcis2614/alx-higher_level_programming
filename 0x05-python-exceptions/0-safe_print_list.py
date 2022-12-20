7f8b65901af8c81a8fa5f0b5c5c23c4488f09747
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-12-19 18:11:06 -0800

    exceptions now

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    string = ""
+    i = 0
+    for i in range(x):
+        try:
+            string += my_list[i]
+        except TypeError:
+            string += str(my_list[i])
+        except IndexError:
+            i--
+            break
+    print("{}".format(string))
+    return (i + 1)

4d1f3954ffbb30427aa4ea0168565fb95f73326a
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-12-20 07:38:17 -0800

    validation the size attribute

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module contains a code for
+a class with one private attribute, i just hope it will work"""
+
+
+class Square:
+    """This class defines the square"""
+    def __init__(self, size=0):
+        if (type(size) is not int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size

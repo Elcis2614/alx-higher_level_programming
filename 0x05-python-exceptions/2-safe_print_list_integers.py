1bd92f8b932e206d60803f7777a4d3bcde3ee4f7
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-12-20 04:15:30 -0800

    more printing

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    string = ""
     t = 0
     for i in range(x):
         try:
@@ -8,9 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
         except (TypeError, ValueError):
             continue
         else:
-            string += str(my_list[i])
             t += 1
-    if (string != ""):
-        string = int(string)
-        print("{:d}".format(string))
+            print("{:d}".format(my_list[i]), end="")
+    print("")
     return (t)

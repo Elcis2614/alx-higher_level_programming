1ae7f44009edcfd521a26b2e55f1edd7075251d8
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-12-19 19:03:30 -0800

    printing more int

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    string = ""
+    t = 0
+    for i in range(x):
+        try:
+            p = int(my_list[i])
+        except (TypeError, ValueError):
+            continue
+        except IndexError:
+            break
+        else:
+            string += str(my_list[i])
+            t += 1
+    if (string != ""):
+        string = int(string)
+        print("{:d}".format(string))
+    return (t)

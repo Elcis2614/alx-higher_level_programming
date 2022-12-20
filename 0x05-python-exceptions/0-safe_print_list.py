23623f93f3df303c168ed5fc46556fbd952eeaed
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-12-19 18:21:36 -0800

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     string = ""
-    i = 0
+    t = 0
     for i in range(x):
         try:
             string += my_list[i]
+            t += 1
         except TypeError:
             string += str(my_list[i])
+            t += 1
         except IndexError:
-            i -= 1
             break
     print("{}".format(string))
-    return (i + 1)
+    return (t)

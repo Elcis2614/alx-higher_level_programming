cc7fe34e92414f9356c9065a238662f17be5d745
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-12-19 18:15:31 -0800

    handling exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,7 +8,7 @@ def safe_print_list(my_list=[], x=0):
         except TypeError:
             string += str(my_list[i])
         except IndexError:
-            i--
+            i -= 1
             break
     print("{}".format(string))
     return (i + 1)

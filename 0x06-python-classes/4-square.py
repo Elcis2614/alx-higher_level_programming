5c6ebcb5657da22e1cc978fb8f6c792003e9004f
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-12-20 12:30:38 -0800

    property and setter

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,7 +5,16 @@ a class with one private attribute, i just hope it will work"""
 
 class Square:
     """This class defines the square"""
-    def __init__(self, size=0):
+    def __init__(self, size):
+        self.size = size
+
+    @property
+    def size(self):
+        """this function returns the area of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
         if (type(size) is not int):
             raise TypeError("size must be an integer")
         elif (size < 0):
@@ -18,10 +27,3 @@ class Square:
         x = self.__size
         x = x * x
         return x
-
-    def size(self):
-        """Returns the size of the square"""
-        return self.__size
-
-    def size(self, value):
-        self.__init__(self, value)

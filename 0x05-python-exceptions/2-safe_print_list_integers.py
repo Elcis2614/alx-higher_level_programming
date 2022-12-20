bbc75d32acdcd001392714ecf55586d36e7e30c1
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-12-19 19:24:10 -0800

    safe print without index handled

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,8 +7,6 @@ def safe_print_list_integers(my_list=[], x=0):
             p = int(my_list[i])
         except (TypeError, ValueError):
             continue
-        except IndexError:
-            break
         else:
             string += str(my_list[i])
             t += 1

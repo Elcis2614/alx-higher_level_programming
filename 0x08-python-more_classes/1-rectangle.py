901d3e6e304140c1c17eae631f65d83731523050
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2023-01-05 20:28:51 -0800

    rectange 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -16,10 +16,10 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """sets the value"""
-        if (type(value) != int):
+        if (type(value) is not int):
             raise TypeError("width must be an integer")
         elif (value < 0):
-            raise TypeError("width must be >= 0")
+            raise ValueError("width must be >= 0")
         else:
             self.__width = value
 
@@ -31,7 +31,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """sets the value of the height"""
-        if (type(value) != int):
+        if (type(value) is not int):
             raise TypeError("height must be an integer")
         elif (height < 0):
             raise ValueError("height must be >= 0")

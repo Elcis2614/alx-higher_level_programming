fd652959e2f196977dfef920b7e437c63a34e0cf
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2023-01-05 20:38:52 -0800

    more attributes

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -19,7 +19,7 @@ class Rectangle:
         if (type(value) != int):
             raise TypeError("width must be an integer")
         elif (value < 0):
-            raise TypeError("width must be >= 0")
+            raise ValueError("width must be >= 0")
         else:
             self.__width = value
 
@@ -33,7 +33,7 @@ class Rectangle:
         """sets the value of the height"""
         if (type(value) != int):
             raise TypeError("height must be an integer")
-        elif (height < 0):
+        elif (value < 0):
             raise ValueError("height must be >= 0")
         else:
             self.__height = value

a34fc31f86414209ce39f42ec5aeaa8375e4bd3b
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2023-01-10 07:37:40 -0800

    more methods in rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Rectangle class"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Rectangle class with height and width"""
+    def __init__(self, width, height):
+        """Initiate the object"""
+        if(self.integer_validator("height", height)):
+            self.__height = height
+        if(self.integer_validator("width", width)):
+            self.__width = width
+
+    def area(self):
+        """Returns the area"""
+        return self.__width * self.__height
+
+    def __str__(str):
+        """returns a printable format of the rectangle """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)

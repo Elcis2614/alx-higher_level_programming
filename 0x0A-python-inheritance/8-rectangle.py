a5cda2c2fe814fed8139725b42c156d35a66d2e9
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2023-01-10 02:49:19 -0800

    rectangel

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,21 +1,12 @@
 #!/usr/bin/python3
 """ Rectangle class"""
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """ Rectangle class with height and width"""
     def __init__(self, width, height):
         """Initiate the object"""
-        try:
-            self.integer_validator(height)
-        except Exception as e:
-            return
-        else:
+        if(self.integer_validator("height", height)):
             self.__height = height
-
-        try:
-            self.integer_validator(width)
-        except Exception as e:
-            return
-        else:
+        if(self.integer_validator("width", width)):
             self.__width = width

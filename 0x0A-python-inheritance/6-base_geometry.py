1ee9d771085f38d50a31991357ef9315a507ed4c
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2023-01-09 23:59:29 -0800

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""New class"""
+
+
+class BaseGeometry:
+    """Base geometry class"""
+    def area(self):
+        """returns a string"""
+        raise Exception("area() is not implemented")

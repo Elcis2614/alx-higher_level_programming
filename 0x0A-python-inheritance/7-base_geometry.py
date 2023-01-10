5138edb908050a9bb91fddbb742c000c5a10655e
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2023-01-10 00:15:01 -0800

    base class

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""New class"""
+
+
+class BaseGeometry:
+    """Base geometry class"""
+    def area(self):
+        """returns a string"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates the value"""
+        if (type(value) != int):
+            raise TypeError("{} must be an integer".format(str(value)))
+        elif (value <= 0):
+            raise ValueError("{} must be greater than 0".format(str(value)))

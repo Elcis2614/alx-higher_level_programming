a450395ac167adc14620759180d15251ce9a9aad
elcis2614 <elisereflorbusole@gmail.com>
2023-06-26 08:26:07 +0000

    welcome to sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,3 @@
 -- gives description of the table first_table
 -- Database name given as an argument
--- SHOW FULL COLUMNS FROM first_table;
-
-SELECT CCSA.character_set_name FROM information_schema.`TABLES` T,information_schema.`COLLATION_CHARACTER_SET_APPLICABILITY` CCSA WHERE CCSA.collation_name = T.table_collation AND T.table_schema = "hbtn_0c_0" AND T.table_name = "first_table";
+SHOW FULL COLUMNS FROM first_table;

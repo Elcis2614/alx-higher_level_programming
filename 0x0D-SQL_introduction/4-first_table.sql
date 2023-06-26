19680ce04f46dbccd57cc630423686678e277b4e
elcis2614 <elisereflorbusole@gmail.com>
2023-06-26 08:08:45 +0000

    welcome to sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- creates first table : first_table
 -- Database name given as an argument
-CREATE TABLE IF NOT EXIST fist_table (
+CREATE TABLE IF NOT EXISTS fist_table (
 	id INT,
 	name VARCHAR(256));

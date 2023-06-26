d7e5e71e8739e4500957f390b242a95371fbab58
elcis2614 <elisereflorbusole@gmail.com>
2023-06-26 12:06:52 +0000

    move to ufc

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,5 @@
 -- converts hbtn_0c_0 database to UTF8
 -- The database name will be passed as an argument
 
-ALTER DATABASE EXOS CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;

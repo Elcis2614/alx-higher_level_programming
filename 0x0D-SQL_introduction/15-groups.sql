9ab9222bf539f9bac018dc767c50c64d8593ddfe
elcis2614 <elisereflorbusole@gmail.com>
2023-06-26 09:30:20 +0000

    order by number

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,6 +1,6 @@
 -- lists the number of records with the same score in the table second_table
 -- Database name given as an argument
-SELECT score, count(score) as nummber
+SELECT score, count(score) as number
 FROM second_table
 GROUP BY score
-ORDER BY score DESC;
+ORDER BY number DESC;

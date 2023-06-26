4f0580b3e878e17d85add23e823b2b7f04874e33
elcis2614 <elisereflorbusole@gmail.com>
2023-06-26 12:08:47 +0000

    move to utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,4 +2,4 @@
 -- The database name will be passed as an argument
 
 
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_general_ci;

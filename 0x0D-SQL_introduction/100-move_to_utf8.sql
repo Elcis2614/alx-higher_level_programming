35a63a637265c692218a9f7f857d980bcfd6a8e0
elcis2614 <elisereflorbusole@gmail.com>
2023-06-27 10:27:54 +0000

    move to utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,5 @@
 -- converts hbtn_0c_0 database to UTF8
 -- The database name will be passed as an argument
-ALTER DATABASE EXOS CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table id int(11);
+ALTER TABLE CHANGE name VARCHAR(256) CHARACTER SET uft8mb4 COLLATE utf8mb4_unicode_ci;

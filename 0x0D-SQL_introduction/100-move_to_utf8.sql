af19a117e4f4440de799e60e40cc46bbad2b6d69
elcis2614 <elisereflorbusole@gmail.com>
2023-06-27 10:09:15 +0000

    larger data

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,3 +2,4 @@
 -- The database name will be passed as an argument
 ALTER DATABASE EXOS CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table id int(11);
--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,6 +1,7 @@
 --  displays the top 3 of cities temperature during July and August ordered by temperature (descending)
-SELECT city, avg(value) as avg_temp LIMIT 3
+SELECT city, avg(value) as avg_temp
 FROM temperatures
-WHERE month = 7 or month = 8
+WHERE month in (7,8)
 GROUP BY city
-ORDER BY avg_temp DESC;
+ORDER BY avg_temp DESC
+LIMIT 3;

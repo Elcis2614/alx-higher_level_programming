fa31542d0696dd9a32276e1eb4bd90d64f5caf4e
elcis2614 <elisereflorbusole@gmail.com>
2023-06-30 01:26:19 +0000

    priviledges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on localhost
 -- If it changes anything
-FLUSH STATUS;
+UPDATE mysql.user SET Super_Priv='Y' WHERE user='user_0d_1' OR user='user_0d_1' AND host='localhost';
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';

f1bd5118dda12439f6bd5cbb52b56612ac74368c
elcis2614 <elisereflorbusole@gmail.com>
2023-07-01 20:02:22 +0000

    new user

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,6 +1,6 @@
 -- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on localhost
 -- If it changes anything
 FLUSH PRIVILEGES;
-SHOW GRANTS FOR 'user_0d_1'@'localhost' USING ALL;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,2 @@
+-- Creates the user user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1_pwd'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';

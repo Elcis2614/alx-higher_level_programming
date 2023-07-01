59563b0f97f6b8e34563b6e5c70cbce2d9ecf0cd
elcis2614 <elisereflorbusole@gmail.com>
2023-07-01 04:55:15 +0000

    list privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,6 @@
 -- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on localhost
 -- If it changes anything
-UPDATE mysql.user SET Super_Priv='Y' WHERE user='user_0d_1' OR user='user_0d_1' AND host='localhost';
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
+SHOW GRANTS FOR 'user_0d_1'@'localhost' USING ALL;
+FLUSH PRIVILEGES;
 SHOW GRANTS FOR 'user_0d_2'@'localhost';

8bff6d9c3ea81377244ddb6722e88c56889980a1
elcis2614 <elisereflorbusole@gmail.com>
2023-07-02 23:19:52 +0000

    privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,6 +1,5 @@
 -- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on localhost
 -- If it changes anything
 FLUSH PRIVILEGES;
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS/G FOR 'user_0d_1'@'localhost';
+SHOW GRANTS/G FOR 'user_0d_2'@'localhost';

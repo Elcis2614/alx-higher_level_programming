2bff5534dfae0acabcd4c395fc953d72c3347a0c
elcis2614 <elisereflorbusole@gmail.com>
2023-07-02 16:16:40 +0000

    create user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,5 @@
 -- Creates the data base hbtn_0d_2 and the USER user_0d_2
 -- the user user_0d_2 has only the SELECT privilege in the database hbtn_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-DROP USER IF EXISTS 'user_0d_2_pwd'@'localhost';
-CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';

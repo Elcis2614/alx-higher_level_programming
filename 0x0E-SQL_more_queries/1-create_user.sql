496a226ee3c4eef2aebe1bbd606b61bbd6114441
elcis2614 <elisereflorbusole@gmail.com>
2023-07-02 14:00:21 +0000

    create user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
--- Creates the user user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1_pwd'@'%' IDENTIFIED BY 'user_0d_1_pwd';
+-- Creates the user user_0d_1 , and grant all privileges
+CREATE USER IF NOT EXISTS 'user_0d_1' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.* TO 'user_0d_1' WITH GRANT OPTION;

fac692b73a5e9e9df3670ca0341b8fa53f7e2e95
elcis2614 <elisereflorbusole@gmail.com>
2023-07-03 00:41:41 +0000

    unique key

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,6 @@
--- creates the table id_not_null on your MySQL server and is unique
+-- creates the table unique_id on your MySQL server and is unique
 -- The database name will be passed as an argument of the mysql command
-CREATE TABLE IF NOT EXISTS id_not_null(
+CREATE TABLE IF NOT EXISTS unique_id(
 	id INT DEFAULT 1,
 	name VARCHAR(256),
-	UNIQUE(id));
+	UNIQUE (id));

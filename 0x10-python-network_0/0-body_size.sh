6668ff7885a37f4494d69eb20125647c2b1299b2
root <elisereflorbusole@gmail.com>
2023-07-05 19:57:13 -0700

    size of the body

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+#curl gets only the header response , grep gets the centent-length line and awk separes the value from the name
 curl -sI $1 | grep -iF content-length | awk -F' ' '{print $(NF) }'

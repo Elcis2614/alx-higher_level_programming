24835ad7400987adf556034f16a4cf41fd5f4059
root <elisereflorbusole@gmail.com>
2023-07-06 00:35:51 -0700

    peak element

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -12,7 +12,7 @@ def find_peak(list_of_integers=[]):
         return None
 
     else:
-        for i in range(len(list_of_integers) - 1):
+        for i in range(len(list_of_integers)):
             if (list_of_integers[i] >= list_of_integers[i - 1]
                     and list_of_integers[i] >= list_of_integers[i + 1]):
                 return list_of_integers[i]
